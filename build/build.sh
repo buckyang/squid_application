87c75a2a50609663a1ec4dfc9cbebc1f11a8fead
buckyang <yxj_yman@sina.com>
2019-06-01 19:14:10 +0800

    c

--- build/build.sh
+++ build/build.sh
@@ -44,8 +44,8 @@ popd > /dev/null
 
 #1. build
 pushd $PROJECT_ROOT_DIR > /dev/null
-sudo docker build --tag="docker-registry.buckyang.com:5050/node-$PROJECT_NAME:latest" 
-sudo docker push docker-registry.buckyang.com:5050/node-$PROJECT_NAME:latest
+sudo docker build --tag="docker-registry.buckyang.com:5050/node-$PROJECT_NAME:latest" .
+#sudo docker push docker-registry.buckyang.com:5050/node-$PROJECT_NAME:latest
 popd > /dev/null	
 
 

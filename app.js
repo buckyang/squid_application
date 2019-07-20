c3260a825702b3da3f36b4ba708883c5b4978249
buckyang <yxj_yman@sina.com>
2019-07-20 17:05:47 +0800

    update

--- app.js
+++ app.js
@@ -1,55 +1,36 @@
-const puppeteer = require('puppeteer');
 const express = require('express');
 const logger = require('./util/logger');
-const { Cluster } = require('puppeteer-cluster');
+const exec = require('child_process').exec;
+const fs = require('fs');
 
 const app = express();
 
 (async () => {
-  const cluster = await Cluster.launch({
-      concurrency: Cluster.CONCURRENCY_PAGE,
-      maxConcurrency: process.env.PUPPETEER_MAX_CONCURRENCY? parseInt(process.env.PUPPETEER_MAX_CONCURRENCY): 1,
-      puppeteerOptions: {args: ['--no-sandbox', '--disable-setuid-sandbox']}
-  });
-  await cluster.task(async ({ page, data: query, worker }) => {
-    let startTime = new Date().getTime();
 
-
-    page.setViewport({
-        width: (query.width? parseInt(query.width): 375),
-        height: (query.height? parseInt(query.height): 667),
-        deviceScaleFactor: (query.deviceScaleFactor ? parseFloat(query.deviceScaleFactor): 1.5)
-    });    
-    await page.goto(query.url);
-    let screenshotResult = page.screenshot({fullPage: true, quality: (query.quality? parseInt(query.quality): 85), type: 'jpeg'});
-
-
-    let endTime = new Date().getTime();
-    logger.info(`worker: ${worker.id}, query: ${query.url? query.url: ''}, generate screenshot cost ${endTime - startTime} millisecond`)
-
-
-    return screenshotResult;
-  });
 
   // setup server
-  app.get('/screenshot/:name', async function (req, res) {
-      if (!req.query.url) {
-          return res.end('Please specify url like this: ?url=example.com');
-      }
-      try {
-          const screen = await cluster.execute(req.query);
-          res.writeHead(200, {
-              'Content-Type': 'image/jpg',
-              'Content-Length': screen.length
-          });
-          res.end(screen);
-
-      } catch (err) {
-        logger.error(`query: ${JSON.stringify(req.query)}, error: ${err.message}`)
-        res.end('Error: ' + err.message);
-      }
+  app.get('/', async function (req, res) {
+    res.end('Hello Wolrd');
   });
 
+
+  app.get('/changeVps', async function(req, res){
+    let confFile = './conf/squid.conf'
+    // /etc/squid/squid.conf
+    let confFileDest = 'd:/squid.conf'
+    let squidConfStr = fs.readFileSync(confFile, 'utf-8')
+    logger.info(`${squidConfStr}`)
+    squidConfStr = squidConfStr.replace('{{sourceServer}}', req.ip);
+    fs.writeFileSync(confFileDest, squidConfStr)
+    // systemctl restart squid
+    exec('echo hello',(error, stdout, stderr) => {
+        if(error){
+            res.end(`Call error: JSON.stringify(Error)`)
+        }else{
+            res.end(`Call success: ${stdout}`)
+        }
+    })
+  })
   app.listen(3000, function () {
       logger.info('Screenshot server listening on port 3000.');
   });
